24899a0f22436d70ab51ee2eef5782165903f199
SantosDavid <daviddsantosd@gmail.com>
2017-08-13 00:05:19 -0300

    Version 1.1

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,22 @@
 |
 */
 
-Route::get('/api/list/vendors', 'VendorsController@listVendors');
+Route::group(['prefix' => 'api/list', 'middleware' => 'auth:api'], function(){
 
-Route::get('/api/list/vendor/{id}', 'VendorsController@listVendor');
+    Route::get('/vendors', 'VendorsController@listVendors');
 
-Route::get('/api/list/sales', 'SalesController@listSales');
+    Route::get('/vendor/{id}', 'VendorsController@listVendor');
 
-Route::get('/api/list/sale/{id}', 'SalesController@listSale');
+    Route::get('/sales', 'SalesController@listSales');
 
-Route::post('/api/list/new/sale', 'SalesController@insert');
+    Route::get('/sale/{id}', 'SalesController@listSale');
+
+    Route::post('/new/sale', 'SalesController@insert');
+});
+
+Route::get('/login', array(
+    'as' => 'login',
+    function() {
+        return response()->json('Acesso negado', 403);
+    }
+));
